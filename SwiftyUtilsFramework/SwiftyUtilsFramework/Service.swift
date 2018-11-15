ac3082bea700ed845cb95691715674cee68ab766
Rashid <rashid.abbas@worldcall.pk>
2018-11-15 11:27:38 +0500

    Added SwiftyFramework

---
+++ SwiftyUtilsFramework/SwiftyUtilsFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  SwiftyUtilsFramework
+//
+//  Created by Rashid Abbas on 15/11/2018.
+//  Copyright © 2018 Rashid Abbas. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Hello Swifty Utils Framework..."
+    }
+}
---
+++ SwiftyUtilsFramework/SwiftyUtilsFramework/SwiftyUtilsFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyUtilsFramework.h
+//  SwiftyUtilsFramework
+//
+//  Created by Rashid Abbas on 15/11/2018.
+//  Copyright © 2018 Rashid Abbas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyUtilsFramework.
+FOUNDATION_EXPORT double SwiftyUtilsFrameworkVersionNumber;
+
+//! Project version string for SwiftyUtilsFramework.
+FOUNDATION_EXPORT const unsigned char SwiftyUtilsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyUtilsFramework/PublicHeader.h>
+
+
